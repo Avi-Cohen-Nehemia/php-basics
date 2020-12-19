928e99573b32f9feec507fde2dad1629a5bc6590
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-12-19 14:05:44 +0000

    cover if statements and its different versions and for loops

--- hello.php
+++ hello.php
@@ -26,3 +26,31 @@ var_dump($firstName . $lastName);   // prints: string(17) "Avi Cohen-Nehemia"
 // like in JS, all the falsy values in PHP are the same. The only difference
 // between the 2 is that empty arrays in PHP are also considered falsy
 var_dump(!![]);                     // prints: bool(false)
+
+// if statements in PHP are exactly the same as in JS,
+// only difference is the usage of dollar signs
+$x = 10;
+
+if ($x > 10) {
+    var_dump('less than 10');
+} else if ($x < 10) {
+    var_dump('more than 10');
+} else {
+    var_dump('is 10');
+}
+
+// We can use ternary and switch
+$x = 10;
+$y = $x > 10 ? 5 : 8;
+var_dump($y);                       // prints: int(8)
+
+switch ($x) {
+    case 9: var_dump('Fish'); break;
+    case 10: var_dump('Wombat'); break;
+    default: var_dump('Blah');
+}
+
+// for loops are the same as well
+for ($i = 0; $i < 10; $i += 1) { 
+    var_dump($i);
+}
