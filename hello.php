086851746bb71d0b9d51960b3b0585c38c8fe468
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-12-19 13:53:17 +0000

    cover php falsy and truthy

--- hello.php
+++ hello.php
@@ -5,12 +5,10 @@
 echo 'Hello, world!';               // prints: 'Hello, world!'
 echo true;                          // prints: '1'
 
-
 // a better way to print values to the console is using var_dump()
 var_dump(true);                     // prints: bool(true)
 var_dump('string');                 // prints: string(6) 'string'
 var_dump(100);                      // prints: int(100)
-
 
 // we refer to variables in PHP using the $ sign
 $animal = 'wombat';
@@ -20,8 +18,11 @@ $name = 'Avi';
 var_dump("Hello {$name}!");         // prints: string(10) "Hello Avi!"
 var_dump('Hello {$name}!');         // prints: string(14) "Hello {$name}!"
 
-
 // in order for us to use concatenation we use . (not + like in JS)
 $firstName = 'Avi';
 $lastName = ' Cohen-Nehemia';
 var_dump($firstName . $lastName);   // prints: string(17) "Avi Cohen-Nehemia"
+
+// like in JS, all the falsy values in PHP are the same. The only difference
+// between the 2 is that empty arrays in PHP are also considered falsy
+var_dump(!![]);                     // prints: bool(false)
