fc88d9fc256d9d56a362a33f5e3dc0506f546062
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-12-19 13:44:06 +0000

    cover php concatenation

--- hello.php
+++ hello.php
@@ -2,16 +2,14 @@
 
 // echo always returns a string so for example if we will
 // try to echo the boolean true, we will get '1' back
-
-echo 'Hello, world!';         // prints: 'Hello, world!'
-echo true;                    // prints: '1'
+echo 'Hello, world!';               // prints: 'Hello, world!'
+echo true;                          // prints: '1'
 
 
 // a better way to print values to the console is using var_dump()
-
-var_dump(true);               // prints: bool(true)
-var_dump('string');           // prints: string(6) 'string'
-var_dump(100);                // prints: int(100)
+var_dump(true);                     // prints: bool(true)
+var_dump('string');                 // prints: string(6) 'string'
+var_dump(100);                      // prints: int(100)
 
 
 // we refer to variables in PHP using the $ sign
@@ -19,5 +17,11 @@ $animal = 'wombat';
 
 // in PHP we can interpolate only when using double quotes
 $name = 'Avi';
-var_dump("Hello {$name}!");   // prints: string(10) "Hello Avi!"
-var_dump('Hello {$name}!');   // prints: string(14) "Hello {$name}!"
+var_dump("Hello {$name}!");         // prints: string(10) "Hello Avi!"
+var_dump('Hello {$name}!');         // prints: string(14) "Hello {$name}!"
+
+
+// in order for us to use concatenation we use . (not + like in JS)
+$firstName = 'Avi';
+$lastName = ' Cohen-Nehemia';
+var_dump($firstName . $lastName);   // prints: string(17) "Avi Cohen-Nehemia"
