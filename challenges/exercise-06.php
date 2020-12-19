34c0632fe01bd1551e2721feac69208e035bdf8b
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-12-19 14:39:34 +0000

    solved exercise 6

---
+++ challenges/exercise-06.php
@@ -0,0 +1,9 @@
+<?php
+
+// Write some code that outputs any even numbers between 1 and 50 that are also divisible by 3
+
+for ($i = 2; $i <= 50 ; $i += 2) {
+    if ($i % 3 === 0) {
+        var_dump($i);
+    }
+}
