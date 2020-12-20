7fb34eab099d974582cfed159430ce7fa9de4742
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-12-20 15:14:16 +0000

    covered foreach

--- arrays-and-functions/cheatSheet.php
+++ arrays-and-functions/cheatSheet.php
@@ -44,3 +44,12 @@ var_dump($assoc['age']);
 // we can add another key value pair to an exiting associative array like so:
 $assoc['nationality'] = 'Israeli';
 var_dump($assoc['nationality']);
+
+// when using foreach loops in php we pass the array we want to loop over first and then
+// the variables that will allow as to access the keys and their values in the loop
+foreach ($assoc as $key => $value) {
+    var_dump("${key}: ${value}");
+};
+
+// generally the native php map, filter and reduce functions are bad to work with.
+// instead we can use libraries from composer or Laravel which provide much better methods to do those things
